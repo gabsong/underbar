9525cd69a25d963dde2670b8e844cdf98fd0c4c3
Gabriel Song <gabsong4@gmail.com>
2019-11-01 21:25:23 -0700

    Implement _.contains function for objects

--- src/underbar.js
+++ src/underbar.js
@@ -189,12 +189,16 @@
   _.contains = function (collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function (wasFound, item) {
-      if (wasFound) {
-        return true;
-      }
-      return item === target;
-    }, false);
+    if (Array.isArray(collection)) {
+      return _.reduce(collection, function(wasFound, item) {
+        if (wasFound) {
+          return true;
+        }
+        return item === target;
+      }, false);
+    } else {
+      return Object.values(collection).indexOf(target) !== -1;
+    }
   };
 
 
